4d0e6d59627122ebc2116adb205fdad5855e9b34
Jose Miguel <jmgbenayas@gmail.com>
2019-08-05 16:12:36 +0200

    Add messages in constants.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from instance import Instance
-
+# Graphs paths
 GRAPH_1_PATH = 'sets/set-a/random-1.txt'
+# Messages
+# TODO ADD LOGGER
+MAIN_INFO_ADJACENTS = 'Node {} and Node {} are adjacents.'
+MAIN_INFO_NOT_ADJACENTS = 'Node {} and Node {} aren\'t adjacents.'
+
 
 if __name__ == '__main__':
     graph = Instance()
@@ -8,6 +13,6 @@ if __name__ == '__main__':
     node_compare = graph.get_nodes().get(0)
     for node in graph.get_nodes().values():
         if node.is_adjacent(node_compare):
-            print('{} and {} are adjacents.'.format(node.get_node_id(), node_compare.get_node_id()))
+            print(MAIN_INFO_ADJACENTS.format(node.get_node_id(), node_compare.get_node_id()))
         else:
-            print('{} and {} aren\'t adjacents.'.format(node.get_node_id(), node_compare.get_node_id()))
+            print(MAIN_INFO_NOT_ADJACENTS.format(node.get_node_id(), node_compare.get_node_id()))
