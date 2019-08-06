64dc4a3ceb21b28f6e8444854cc7be2275ce9d1e
Jose Miguel <jmgbenayas@gmail.com>
2019-08-06 18:29:34 +0200

    Add logger

---
+++ logger/logger.py
@@ -0,0 +1,11 @@
+import logging
+import sys
+
+
+class Logger:
+    LEVEL = logging.DEBUG
+
+    @staticmethod
+    def init_log():
+        logging.basicConfig(stream=sys.stdout, format='%(asctime)s\t%(levelname)s\t%(name)s\t%(message)s', level=Logger.LEVEL)
+        logging.info('Logging started')
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from instance import Instance
-import time
 from time import gmtime, strftime
+import logging
+
+from logger.logger import Logger
+
+# Logs
+Logger.init_log()
+LOGGER = logging.getLogger(__name__)
 
 # Graphs paths
 GRAPH_1_PATH = 'sets/set-a/random-1.txt'
 # Messages
-# TODO ADD LOGGER
 MAIN_INFO_ADJACENTS = 'Node {} and Node {} are adjacents.'
 MAIN_INFO_NOT_ADJACENTS = 'Node {} and Node {} aren\'t adjacents.'
 MAIN_INFO_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
@@ -14,10 +19,10 @@ if __name__ == '__main__':
     graph = Instance()
     graph.read_file(GRAPH_1_PATH)
     node_compare = graph.get_nodes().get(0)
-    print(strftime(MAIN_INFO_TIME_FORMAT, gmtime()))
+    LOGGER.debug(strftime(MAIN_INFO_TIME_FORMAT, gmtime()))
     for node in graph.get_nodes().values():
         if node.is_adjacent(node_compare):
-            print(MAIN_INFO_ADJACENTS.format(node.get_node_id(), node_compare.get_node_id()))
+            LOGGER.debug(MAIN_INFO_ADJACENTS.format(node.get_node_id(), node_compare.get_node_id()))
         else:
-            print(MAIN_INFO_NOT_ADJACENTS.format(node.get_node_id(), node_compare.get_node_id()))
-    print(strftime(MAIN_INFO_TIME_FORMAT, gmtime()))
+            LOGGER.debug(MAIN_INFO_NOT_ADJACENTS.format(node.get_node_id(), node_compare.get_node_id()))
+    LOGGER.debug(strftime(MAIN_INFO_TIME_FORMAT, gmtime()))
