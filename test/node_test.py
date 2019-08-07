f7de16f7c74985dab3e3e6fcf2708e307e13135a
Jose Miguel <jmgbenayas@gmail.com>
2019-08-07 18:39:26 +0200

    Add test for a read_file method

--- test/node_test.py
+++ test/node_test.py
@@ -1,9 +1,17 @@
 import unittest
 
+from instance import Instance
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+
+class InstanceTest(unittest.TestCase):
+    GRAPH_TEST_PTH = 'test/test_files/test-random-1.txt'
+
+    def test_read_file_OK(self):
+        graph = Instance()
+        graph.read_file(InstanceTest.GRAPH_TEST_PTH)
+        self.assertEqual(graph.get_total_nodes(), '100')
+        self.assertEqual(graph.get_total_edges(), '2266')
+        self.assertEqual(len(graph.get_nodes()), 100)
 
 
 if __name__ == '__main__':
