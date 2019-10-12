8855867282b289cff8bca6d305cc6bc9f873f31b
Jose Miguel Garcia <jmgbenayas@gmail.com>
2019-10-12 12:44:35 +0200

    Add parcial log processing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Created by José Miguel García Benayas
 
 import logging
-import os
+import os, sys
 
 from instance import Instance
 from logger.logger import Logger
@@ -20,6 +20,7 @@ GRAPH_PATH = 'sets/set-a/'
 
 # Messages
 MAIN_TOTAL_TIME = "Total time: {:1.10f} seconds"
+MAIN_PROCESS_FILE = "Processing: "
 MAIN_PARTIAL_TIME = "{} with {} cliques : {:1.10f} seconds"
 
 if __name__ == '__main__':
@@ -29,6 +30,7 @@ if __name__ == '__main__':
     for file in os.listdir(GRAPH_PATH):
         graph = Instance()
         graph.read_file(GRAPH_PATH + file)
+        print(MAIN_PROCESS_FILE + file)
         solution = Solution(graph, os.path.splitext(file)[0])
         solution.set_solution_max_cliques()
         print(MAIN_PARTIAL_TIME.format(os.path.splitext(file)[0], len(solution.cliques), solution.compute_time))
@@ -36,4 +38,4 @@ if __name__ == '__main__':
 
     GraphUtils.export_solution(data)
     LOGGER.debug(MAIN_TOTAL_TIME.format(time.time() - start_time))
-
+    sys.exit()
